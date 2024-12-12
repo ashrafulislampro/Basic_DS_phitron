3bee8c8b55bb998f7497d8be3f2eef605653c1df
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-12-12 06:58:30 +0600

    practice problem-1 is solved

---
+++ Module-3/running_sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> runningSum(n);
+    runningSum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        runningSum[i] = runningSum[i - 1] + arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << runningSum[i] << " ";
+    }
+    return 0;
+}
